320a221dfc5f76fe055e191047dbed84283e28d6
samtasticc <samantha.henesy@gmail.com>
2025-01-23 20:54:23 -0500

    add CSS

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    # return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse('<h1>About CatCollector</h1>')
+    return render(request, 'about.html')
+    # return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
