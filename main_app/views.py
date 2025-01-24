15bd1be3da5044d843779c934b94c2289a8958c8
samtasticc <samantha.henesy@gmail.com>
2025-01-23 21:32:25 -0500

    fix what i broke related to CSS

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name,
+        self.breed = breed,
+        self.description = description,
+        self.age = age
+        
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
 
 def home(request):
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
@@ -10,3 +22,6 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
     # return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
+    def cat_index(request):
+        return render(request, 'cats/index.html', {'cats': cats})
